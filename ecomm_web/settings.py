6a8ffe52caa7d151e17ee9cdb38f3dae971c9e6a
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2023-05-01 21:36:18 +0500

    update settings

--- ecomm_web/settings.py
+++ ecomm_web/settings.py
@@ -129,13 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Add Manually
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    BASE_DIR / "/tmp/build_8d960f00/static/",
 ]
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, "media")
 MEDIA_URL="/media/"
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
